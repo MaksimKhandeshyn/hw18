8503c0ee8cdf79942b0e39e46b84ebdb5b5f0db2
MaksimKhandeshyn <145330399+MaksimKhandeshyn@users.noreply.github.com>
2025-02-25 22:15:49 +0200

    make reduce obj

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//--1--
+const categories = document.querySelectorAll("#animals .li-item");
+console.log(`У списку ${categories.length} категорії.`);
+
+console.log(categories);
+
+const titleEl = document.querySelectorAll("#animals .li-item");
+
+const secondEl = document.querySelectorAll("ul li");
+console.log(secondEl);
+secondEl.forEach((element) => console.log(element));
